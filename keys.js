b61248bc530e5e4d8b1347c098ed1b572e34498f
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-01-14 14:51:49 -0700

    second commit

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.bit = {
+    key: process.env.BIT_Key
+};
+
+exports.omdb = {
+    key: process.env.OMDB_Key
+};
--- liri.js
+++ liri.js
@@ -1,22 +1,32 @@
+// Read and set any environment variables with the dotenv package //
 // Require the .env file with access tokens //
-require('dotenv').config();
+require("dotenv").config();
+
 
 // ---- SPOTIFY ---- //
-const spotify = require('./commands/spotify');
+const spotify = require("./commands/spotify");
 
 // ---- BANDS IN TOWN ---- //
-const bands = require('./commands/bands');
+const bands = require("./commands/bands");
 
 // ---- OMDB ---- //
-const omdb = require('./commands/omdb');
+const omdb = require("./commands/omdb");
 
 // ---- DO WHAT IS SAYS ---- //
-const doWhatItSays = require('./commands/do-what-it-says');
+const doWhatItSays = require("./commands/do-what-it-says");
 
 // ---- HELP INFO ---- //
-const help = require('./commands/help');
+const help = require("./commands/help");
+
+
+// Required to import the `keys.js` file and store it in a variable //
+let keys = require("./keys.js");
+
+// Access your keys information //
+let spotify = new Spotify(keys.spotify);
+
 
 // ---- COMMANDS/LOGGER/OPTIONS ---- //
 // Require only log_command from logger module //
-const {log_command} = require('./commands/logger');
+const {log_command} = require("./commands/logger");
 let command = process.argv[2];
